c5ef3814c477d87edef8d48f0b98339fac7df285
honehone12 <54838229+honehone12@users.noreply.github.com>
2022-04-03 21:19:10 +0900

    simple controller

--- FPSProject/FPSProjectGameModeBase.cpp
+++ FPSProject/FPSProjectGameModeBase.cpp
@@ -3,3 +3,16 @@
 
 #include "FPSProjectGameModeBase.h"
 
+void AFPSProjectGameModeBase::StartPlay()
+{
+	Super::StartPlay();
+
+	check(GEngine != nullptr);
+
+	GEngine->AddOnScreenDebugMessage(
+		-1,
+		5.0f,
+		FColor::Yellow,
+		TEXT("Hello UnrealEngine, this is FPSGameMode !!")
+	);
+}
